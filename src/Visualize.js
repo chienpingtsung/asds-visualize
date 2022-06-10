4497c2d8a53fb1eb78f596488dc1ec01241a2c5f
Chienping Tsung <jianpingzong@outlook.com>
2022-06-10 12:45:16 +0800

    remove strict mode
    the strict mode from react could cause twice render for appendChild func, I have no idea about why.

--- src/Visualize.js
+++ src/Visualize.js
@@ -1,10 +1,35 @@
 import {Component} from "react";
+import * as THREE from "three";
 
 class Visualize extends Component {
+    componentDidMount() {
+        this.initScene();
+    }
+
+    initScene() {
+        const container = document.getElementById("container");
+
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        container.appendChild(renderer.domElement);
+
+        const scene = new THREE.Scene();
+
+        const camera = new THREE.PerspectiveCamera();
+
+        function animate() {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        }
+
+        animate();
+    }
+
     render() {
         return(
             <div>
-                Hello, world!
+                <div id="container"></div>
             </div>
         );
     }
