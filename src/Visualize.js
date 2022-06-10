93ac89387c334e592008ec2e3f86fc373157a379
Chienping Tsung <jianpingzong@outlook.com>
2022-06-10 12:15:19 +0800

    init Visualize component

---
+++ src/Visualize.js
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+class Visualize extends Component {
+    render() {
+        return(
+            <div>
+                Hello, world!
+            </div>
+        );
+    }
+}
+
+export default Visualize;
